6ff6ae34a8537ede35237eb14388a17d3d02f4c2
João Tavares <jvst@ic.ufal.br>
2021-07-07 15:03:47 -0300

    Adding timecard insertion and sales posting functionalities.

--- classes/sqlmanager.py
+++ classes/sqlmanager.py
@@ -1,7 +1,8 @@
 import mysql.connector
+from classes import system_postings
 
-#This class provides the management and
-#manipulation of all transactions made with the database.
+# This class provides the management and
+# manipulation of all transactions made with the database.
 class SQLManager:
     def __init__(self):
         self.db = 'payroll'
@@ -10,98 +11,151 @@ class SQLManager:
         self.conn = None
         self.cursor = None
         self.table = 'employees'
-        self.table_atr = ['employee_name','employee_addr','jType','PayMethod','syndicate']
+        self.table_atr = ['employee_name','employee_addr','jType','PayMethod','isInSyndicate']
 
-    #This method is responsible for instantiating the connection to the database as an
-    #object and instantiating our cursor, which will carry out the transactions.
+    # This method is responsible for instantiating the connection to the database as an
+    # object and instantiating our cursor, which will carry out the transactions.
     def databaseConn(self):
         try:
             self.conn = mysql.connector.connect(db=self.db, user=self.user, passwd=self.passwd, port=3307)
             self.cursor = self.conn.cursor()
         except mysql.connector.Error as err:
+            print("----------------------------")
             print("Failed to connect: {}".format(err))
 
-    #This method is responsible for terminating the
-    #connection object and removing the reference to the cursor.
+    # This method is responsible for terminating the
+    # connection object and removing the reference to the cursor.
     def databaseConnClose(self):
         try:
             self.cursor = None
             self.conn.close()
         except mysql.connector.Error as err:
+            print("----------------------------")
             print("Failed to close connection: {}".format(err))
 
-    #Method responsible for performing the insertion operation in a table
-    #in our database, using the parameters specified in the class attributes and received from method call.
+    # Method responsible for performing the insertion operation in a table
+    # in our database, using the parameters specified in the class attributes and received from method call.
     def insertInTable(self, value):
         try:
             self.databaseConn()
             self.cursor.execute((
-                "INSERT INTO employees (employee_name,employee_addr,jType,PayMethod,syndicate)"
-                " VALUES (%s, %s, %s, %s, %s)"
+                "INSERT INTO employees (employee_name,employee_addr,jType,PayMethod,isInSyndicate,"
+                "syndicateCharge, serviceCharge, wage, pointCardId, workedHours, sellCount)"
+                " VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
             ), value)
             self.conn.commit()
+            print("----------------------------")
             print("Successfully inserted")
         except mysql.connector.Error as err:
+            print("----------------------------")
             print("Failed to insert in table: {}".format(err))
             self.conn.rollback()
         finally:
             self.databaseConnClose()
+            print("----------------------------")
             print("Closed connection")
 
-    #Method responsible for performing the remove operation on a table in our database, using the
-    #parameters specified in the class attributes and received from the method call
+    # Method responsible for performing the remove operation on a table in our database, using the
+    # parameters specified in the class attributes and received from the method call
     def deleteFromTable(self, value):
         try:
             self.databaseConn()
             self.cursor.execute((
-                "DELETE FROM employees WHERE employee_name = %s"
+                "DELETE * FROM employees WHERE employee_name = %s"
             ), (value,))
             self.conn.commit()
+            print("----------------------------")
             print("Successfully removed")
         except mysql.connector.Error as err:
+            print("----------------------------")
             print("Failed to delete from table: {}".format(err))
             self.conn.rollback()
         finally:
             self.databaseConnClose()
+            print("----------------------------")
             print("Closed connection")
 
-    #Method that updates an employee's record in our table
-    #up_op is used to decide whether to change our employee's address or work type
-    def updateTable(self, value, up_op=False):
-        if up_op == True:
+    # Method that updates an employee's record in our table
+    # up_op is used to decide whether to change our employee's address or work type
+    def updateTable(self, value, up_op):
+        if up_op == 0:
             update_cmd = (
                 "UPDATE employees SET employee_addr = %s WHERE employee_name = %s"
             )
-        else:
+        elif up_op == 1:
             update_cmd = (
                 "UPDATE employees SET jType = %s WHERE employee_name = %s"
+            )
+        elif up_op == 2:
+            update_cmd = (
+                "UPDATE employees SET employee_name = %s WHERE employee_name = %s"
+            )
+        elif up_op == 3:
+            update_cmd = (
+                "UPDATE employees SET payMethod = %s WHERE employee_name = %s"
+            )
+        elif up_op == 4:
+            update_cmd = (
+                "UPDATE employees SET isInSyndicate = %s WHERE employee_name = %s"
+            )
+        elif up_op == 5:
+            update_cmd = (
+                "UPDATE employees SET syndicateCharge = %s WHERE employee_name = %s"
+            )
+        elif up_op == 6:
+            update_cmd = (
+                "UPDATE employees SET workedHours = %s WHERE employee_name = %s"
             )
         try:
             self.databaseConn()
             self.cursor.execute(update_cmd, value)
             self.conn.commit()
+            print("----------------------------")
             print("Table updated successfully")
         except mysql.connector.Error as err:
+            print("----------------------------")
             print("Failed to update table: {}".format(err))
         finally:
             self.databaseConnClose()
+            print("----------------------------")
             print("Closed connection")
 
-    #Method responsible for performing a name lookup in
-    #our table, the method can be used to acquire information
-    #about an employee to update reassemble the respective employee's class
-    def searchInTable(self, value):
+    # Method responsible for performing a name lookup in
+    # our table, the method can be used to acquire information
+    # about an employee to update reassemble the respective employee's class
+    def searchInTable(self, value, type=None):
+        result = None
+        emplo = None
+        if type == 1:
+            emplo = system_postings.PointCard()
+        elif type == 2:
+            emplo = system_postings.SalePost()
+        elif type == 3:
+            pass
+
         try:
             self.databaseConn()
+            print("aaaa")
             self.cursor.execute((
-                "SELECT * FROM employees WHERE employee_name = %s"
-            ), (value,))
-            result = self.conn.fetchone()
+                "SELECT id, employee_name,employee_addr,jType,PayMethod,isInSyndicate,"
+                "syndicateCharge,serviceCharge,wage,pointCardId,workedHours"
+                " FROM employees WHERE employee_name = %s;"
+            ), (value))
+
+            result = self.cursor.fetchall()
+
+            if type == 1:
+                emplo.constructHourlyFSelect(result)
+            elif type == 2:
+                pass
+            print(emplo.getEmployeeName())
+            print("----------------------------")
             print("Search completed successfully")
         except mysql.connector.Error as err:
-            result = None
+            print("----------------------------")
             print("Failed to search in table: {}".format(err))
         finally:
             self.databaseConnClose()
+            print("----------------------------")
             print("Closed connection")
-            return result
+            return emplo
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import classes.system_postings, datetime
 from classes import employee, sqlmanager
 
 DbManager = sqlmanager.SQLManager()
@@ -10,23 +11,74 @@ if __name__ == '__main__':
         print("----------------------------")
         print("1 - Add a new employee")
         print("2 - Remove an employee")
-        print("3 - EXIT")
+        print("3 - Beat time")
+        print("4 - Launch new sale")
+        print("6 - Update employee record")
+        print("4 - EXIT")
         print("----------------------------")
         x = int(input('Enter your option: '))
         if x == 1:
             name = input('Enter the employee\'s name: ')
             address = input('Enter the employee\'s address: ')
             print("----------------------------")
-            print("--- Summary of job types ---")
+            print("----- Summary of jobs ------")
             print("---------- Hourly ----------")
             print("--------- Salaried ---------")
             print("------- Commissioned -------")
             print("----------------------------")
             j_type = input('Enter the employee\'s job: ')
-            New_Employee = employee.Employee(name, address, j_type)
-            DbManager.insertInTable(New_Employee.getEmployeeAttributes())
+
+            if j_type.lower() == 'hourly':
+                New_Employee = employee.Hourly(name, address)
+                DbManager.insertInTable(New_Employee.getHourlyAttributes())
+            elif j_type.lower() == 'salaried':
+                New_Employee = employee.Salaried(name, address)
+                DbManager.insertInTable(New_Employee.getSalariedAttributes())
+            elif j_type.lower() == 'commissioned':
+                New_Employee = employee.Comissioned(name, address)
+                DbManager.insertInTable(New_Employee.getComissionedAttributes())
         elif x == 2:
             name = input('Enter the name of the employee to be removed: ')
             DbManager.deleteFromTable(name)
+        elif x == 3:
+            name = input('Enter your name: ')
+            Point_card = DbManager.searchInTable(name, 1)
+            print(Point_card.getEmployeeName())
+            arrival = int(input('Enter your time of entry: '))
+            departure = int(input('Enter your departure time: '))
+            DbManager.updateTable(Point_card.postPointCard(arrival,departure), 6)
+        elif x == 4:
+            name = input('Enter your name: ')
+            New_sale = DbManager.searchInTable(name, 2)
+        elif x == 6:
+            e_name = input('Enter your name: ')
+            print("----------------------------")
+            print("---- Summary of Updates ----")
+            print("------- 1 - Address  -------")
+            print("--------- 2 - Type ---------")
+            print("--------- 3 - Name ---------")
+            print("------ 4 - Syndicate -------")
+            print("--- 5 - Syndicate Charge ---")
+            print("---- 6 - Payment method ----")
+            print("----------------------------")
+            y = int(input('Enter option: '))
+            if y == 1:
+                n_addr = input('Enter new address: ')
+                DbManager.updateTable(tuple(n_addr,e_name),0)
+            elif y == 2:
+                n_type = input('Enter new type: ')
+                DbManager.updateTable(tuple(n_type,e_name),1)
+            elif y == 3:
+                n_name = input('Enter new name: ')
+                DbManager.updateTable(tuple(n_name,e_name),2)
+            elif y == 4:
+                n_syndicate = input('Syndicate True or False: ')
+                DbManager.updateTable(tuple(n_syndicate,e_name),4)
+            elif y == 5:
+                charge = input('Enter syndicate charge: ')
+                DbManager.updateTable(tuple(charge,e_name),5)
+            elif y == 6:
+                n_pMethod = input('Enter new payment method: ')
+                DbManager.updateTable(tuple(n_pMethod,e_name),3)
         else:
             break
